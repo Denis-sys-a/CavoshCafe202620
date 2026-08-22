c16a8d44728bbdd4d892ab76683906c7ad97d753
Giovanni Denis <denishilario3@gmail.com>
2026-08-22 15:45:08 -0500

    first commit

---
+++ app/src/main/java/com/senatino/cavoshcafe202620/ui/Carrito.java
@@ -0,0 +1,43 @@
+package com.senatino.cavoshcafe202620.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.support.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.senatino.cavoshcafe202620.R;
+import com.senatino.cavoshcafe202620.databinding.FragmentCarritoBinding;
+
+public class Carrito extends Fragment {
+    FragmentCarritoBinding binding;
+    Context context;
+    NavController navController;
+    View view;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCarritoBinding.inflate(inflater, container, false );
+        return view = binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController( view );
+    }
+}
---
+++ app/src/main/java/com/senatino/cavoshcafe202620/ui/Inicio.java
@@ -0,0 +1,48 @@
+package com.senatino.cavoshcafe202620.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.support.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.senatino.cavoshcafe202620.R;
+import com.senatino.cavoshcafe202620.databinding.FragmentInicioBinding;
+
+public class Inicio extends Fragment {
+
+    FragmentInicioBinding binding;
+    Context context;
+    NavController navController;
+    View view;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentInicioBinding.inflate(inflater, container, false );
+        return view = binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController( view );
+    }
+}
